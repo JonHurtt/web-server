94f5aba3c90a2ac966302c397a5dcd2293469c34
Jonathan R. Hurtt <JonHurtt@gmail.com>
2016-02-28 19:48:22 -0500

    Initialize Repo

---
+++ server.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+//app.get(route, function(request, response))
+/*app.get('/', function(request, response){
+	console.log('Request Made to root directory (\'/\')')	
+	response.send('Hello Express!<br><a href="/about">About</a>');
+});
+*/
+
+var middleware = {
+	requireAuthentication: function(request, response, next){
+		console.log('private route hit');
+		next();
+	},
+	logger: function(request, response, next){
+		console.log('Request: [' + new Date().toString() + '] ' +  request.method + ' ' + request.originalUrl);
+		next()
+	}	
+}
+
+//app.use(middleware.requireAuthentication);
+app.use(middleware.logger);
+
+app.get('/about',middleware.requireAuthentication,  function (request, response){
+	console.log('Request Made to root directory (\'/about\')')	
+	response.send('About Page will be located here...<br><a href="/">Back</a>');
+});
+
+app.use(express.static(__dirname + '/public'));
+
+
+console.log("Starting Web Server...");
+app.listen(PORT, function(){
+	console.log('Express Web Server Started...');
+	console.log('Please visit http://127.0.0.1:'+ PORT + '!');
+
+});
